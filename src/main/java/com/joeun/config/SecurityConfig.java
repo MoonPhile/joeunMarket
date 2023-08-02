c7d6bd069f4cf278ae48de062061c84dd04df6c1
LEEJEHEE <lunatic1702@gmail.com>
2023-08-02 16:21:39 +0900

    fix

--- src/main/java/com/joeun/config/SecurityConfig.java
+++ src/main/java/com/joeun/config/SecurityConfig.java
@@ -16,7 +16,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests().requestMatchers(
+        http.csrf().disable()
+                .authorizeHttpRequests().requestMatchers(
                         new AntPathRequestMatcher("/**")).permitAll()
                 .and()
                 .formLogin()
