be4aa65e92d745777b8efde449a1ba6585d58ee7
허윤서 <pororiya4444@gmail.com>
2023-08-04 12:22:43 +0900

    Merge
    파일 합침

---
+++ src/test/java/com/joeun/CartServiceTests.java
@@ -0,0 +1,11 @@
+//package com.joeun;
+//
+//import com.joeun.service.CartService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class CartServiceTests {
+//    @Autowired
+//    private CartService service;
+//}
