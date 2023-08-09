281f4768c5d67934b7c05bfa3c7ddeaeff44d9ef
LEEJEHEE <lunatic1702@gmail.com>
2023-08-09 16:54:02 +0900

    변수 명 수정 및 불필요한 코드 삭제

--- src/main/resources/static/js/paycancel.js
+++ src/main/resources/static/js/paycancel.js
@@ -1,7 +1,11 @@
-
-
 //결제 취소 요청 함수 예정
 function cancelPay(){
+    //필요한 값 변수에 저장
+    // var price = document.getElementById('price').value;
+    var userId = document.getElementById('userId').value;
+    // var orderId = document.getElementById('ordersId').value;
+    // var productId = document.getElementById('productId').value;
+    // var productName = document.getElementById('productName').value;
     $.ajax({
         url: "/payment/payCancel",
         method: "POST",
@@ -10,7 +14,7 @@ function cancelPay(){
             impUid : imp_uid,            // 결제 고유번호
             orderId: merchant_uid,   // 주문번호
             // paymentPrice: rsp.paid_amount,     // 미입력시 전체 금액 환불
-            userId: 1,      //유저 아이디(구매자)
+            userId: userId,      //유저 아이디(구매자)
         }),
     }).done(function (data) {
         // 가맹점 서버 결제 API 성공시 로직
