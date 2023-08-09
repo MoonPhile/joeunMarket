70a4f39d85c83a1dcb657e8065eb874af09c7db1
허윤서 <pororiya4444@gmail.com>
2023-08-09 11:51:24 +0900

    orders db save complete
    orders db 들어가는 것 확인

--- src/main/java/com/joeun/mapper/OrderMapper.java
+++ src/main/java/com/joeun/mapper/OrderMapper.java
@@ -1,15 +1,17 @@
 package com.joeun.mapper;
 
+
 import com.joeun.dto.OrderDto;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface OrderMapper {
-    void insertOrder(OrderDto order);
+//    void insertOrder(OrderDto orderDto);
+    void saveOrder(OrderDto orderDto);
 
-    List<OrderDto> getAllOrders(); // 주문 목록을 조회하는 메서드
+    List<OrderDto> getAllOrders();
 
-    // 추가적인 메서드 정의
 }
