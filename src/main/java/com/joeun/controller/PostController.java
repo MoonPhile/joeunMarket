8c7dc663e8339383079a66d241e92dd494ed1699
lys0320 <yjjangun98@naver.com>
2023-08-11 12:31:43 +0900

    fix

--- src/main/java/com/joeun/controller/PostController.java
+++ src/main/java/com/joeun/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.joeun.controller;
 
+import com.joeun.dto.PagingResponse;
 import com.joeun.dto.PostFileRequest;
+import com.joeun.dto.PostFileResponse;
+import com.joeun.dto.SearchDto;
 import com.joeun.dto.PostRequest;
 import com.joeun.dto.PostResponse;
 import com.joeun.service.PostFileService;
@@ -10,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,9 +48,9 @@ public class PostController {
     }
     // 게시글 리스트 페이지
     @GetMapping("/list.do")
-    public String openPostList(Model model) {
-        List<PostResponse> posts = postService.findAllPost();
-        model.addAttribute("posts", posts);
+    public String openPostList(@ModelAttribute("params") final SearchDto params, Model model) {
+        PagingResponse<PostResponse> response = postService.findAllPost(params);
+        model.addAttribute("response", response);
         return "post_list";
     }
     // 게시글 상세 페이지
@@ -58,10 +62,29 @@ public class PostController {
     }
     // 기존 게시글 수정
     @PostMapping("/update.do")
-    public String updatePost(final PostRequest params) {
+    public String updatePost(final PostRequest params, Model model) {
+
+        // 1. 게시글 정보 수정
         postService.updatePost(params);
+
+        // 2. 파일 업로드 (to disk)
+        List<PostFileRequest> uploadFiles = fileUtils.uploadFiles(params.getFiles());
+
+        // 3. 파일 정보 저장 (to database)
+        postFileService.saveFiles(params.getId(), uploadFiles);
+
+        // 4. 삭제할 파일 정보 조회 (from database)
+        List<PostFileResponse> deleteFiles = postFileService.findAllFileByIds(params.getRemoveFileIds());
+
+        // 5. 파일 삭제 (from disk)
+        fileUtils.deleteFiles(deleteFiles);
+
+        // 6. 파일 삭제 (from database)
+        postFileService.deleteAllFileByIds(params.getRemoveFileIds());
+
         return "redirect:/list.do";
     }
+
     // 게시글 삭제
     @PostMapping("/delete.do")
     public String deletePost(@RequestParam final Long id) {
