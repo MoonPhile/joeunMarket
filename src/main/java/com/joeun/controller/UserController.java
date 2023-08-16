7508a61c6bb9eb91f0c8e0a92213101696bd9121
jslee1125 <msmile1125@naver.com>
2023-08-16 22:21:35 +0900

    수정테스트
    11

--- src/main/java/com/joeun/controller/UserController.java
+++ src/main/java/com/joeun/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 
     @GetMapping("/login")
     public String login(UserCreateForm userCreateForm){
-        return "/user/login_form";
+        return "user/login_form";
     }
 
 //    @GetMapping("/login")
@@ -33,13 +33,13 @@ public class UserController {
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult){
         if (bindingResult.hasErrors()){
-            return "/user/login_form";
+            return "user/login_form";
         }
 
         if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
             bindingResult.rejectValue("password2","passwordInCorrect",
                     "패스워드가 일치하지 않습니다.");
-            return "/user/login_form";
+            return "user/login_form";
         }
 
         try{
@@ -47,11 +47,11 @@ public class UserController {
         }catch (DataIntegrityViolationException e){
             e.printStackTrace();
             bindingResult.reject("signupFailed","이미 등록된 사용자 입니다.");
-            return "/user/login_form";
+            return "user/login_form";
         }catch (Exception e){
             e.printStackTrace();
             bindingResult.reject("signupFailed",e.getMessage());
-            return "/user/login_form";
+            return "user/login_form";
         }
 
         return "redirect:/";
@@ -63,7 +63,7 @@ public class UserController {
         String userId = authentication.getName(); // 사용자 아이디 가져오기
         User user = userService.getUserById(userId); // 해당 사용자 아이디로 사용자 정보 조회
         model.addAttribute("user", user);
-        return "/user/edituser";
+        return "user/edituser";
     }
 
     @PostMapping("/edit")
